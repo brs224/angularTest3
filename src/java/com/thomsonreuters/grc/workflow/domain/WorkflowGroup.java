b0222e86829d7375e7158d9da78500e7b83b4d6e
Albert.Oneil <albert.oneil@thomsonreuters.com>
2016-10-20 19:09:55 -0400

    Add users and groups POC functionality
    Also includes class refactoring to match strategy

---
+++ src/java/com/thomsonreuters/grc/workflow/domain/WorkflowGroup.java
@@ -0,0 +1,34 @@
+package com.thomsonreuters.grc.workflow.domain;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "workflowGroup")
+public class WorkflowGroup {
+	private  String id;
+	private  String name;
+	private  String type;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
