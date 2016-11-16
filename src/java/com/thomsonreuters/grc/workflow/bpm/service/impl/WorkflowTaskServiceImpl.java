ccddf05b109bc269777e5da0312c7da007cf9455
Smitha Mandicha <smitha.mandicha@thomsonreuters.com>
2016-11-15 19:11:45 -0500

    Activiti Engine Framework

--- src/java/com/thomsonreuters/grc/workflow/bpm/service/impl/WorkflowTaskServiceImpl.java
+++ src/java/com/thomsonreuters/grc/workflow/bpm/service/impl/WorkflowTaskServiceImpl.java
@@ -10,11 +10,6 @@ import java.util.Map;
 
 import com.thomsonreuters.grc.workflow.bpm.service.WorkflowTaskService;
 import com.thomsonreuters.grc.workflow.domain.WorkOrderTask;
-/**
- * Copyright 2016: Thomson Reuters Global Resources. All Rights Reserved.
- * Proprietary and Confidential information of TRGR.
- * Disclosure, Use or Reproduction without the written authorization of TRGR is prohibited.
- */
 import com.thomsonreuters.grc.workflow.workspace.domain.CreateTaskParams;
 import com.thomsonreuters.grc.workflow.workspace.domain.GrcTask;
 import com.thomsonreuters.grc.workflow.workspace.domain.TaskSearchParams;
--- src/java/com/thomsonreuters/grc/workflow/domain/WorkStreamTask.java
+++ src/java/com/thomsonreuters/grc/workflow/domain/WorkStreamTask.java
@@ -8,11 +8,6 @@ package com.thomsonreuters.grc.workflow.domain;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "workStreamTask")
-/**
- * Copyright 2016: Thomson Reuters Global Resources. All Rights Reserved.
- * Proprietary and Confidential information of TRGR.
- * Disclosure, Use or Reproduction without the written authorization of TRGR is prohibited.
- */
 public class WorkStreamTask {
     
     public String taskName;
